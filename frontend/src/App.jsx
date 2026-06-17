fa5062dcaa5be5ee7620da9459056be17f2ee9b4
Nitin Kumar <nk9465110@gmail.com>
2026-06-17 23:47:10 +0530

    signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react"
+import { Route, Routes } from "react-router"
+// import HomePage from "./pages/home/HomePage"
+import SignUpPage from "./pages/auth/signup/SignUpPage"
+// import LoginPage from "./pages/auth/login/LoginPage"
 
 function App() {
   
   return (
-    <>
-      Hello
-    </>
+      <div className='flex max-w-6xl mx-auto'>
+			<Routes>
+				{/* <Route path='/' element={<HomePage />} /> */}
+				<Route path='/signup' element={<SignUpPage />} />
+				{/* <Route path='/login' element={<LoginPage />} /> */}
+			</Routes>
+		</div>
   )
 }
 
