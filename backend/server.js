a05c2c82844baeabac83eae5a9b6a1167c848196
Nitin Kumar <nk9465110@gmail.com>
2026-06-23 17:03:14 +0530

    app deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import { v2 as cloudinary } from 'cloudinary'
+import path from "path"
 
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
@@ -17,8 +18,8 @@ cloudinary.config({
 })
 
 const app = express()
-
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 app.use(express.json({limit : "5mb"}))  // to parse req.body (limit should not be too high to prevent DOS attack)
 app.use(express.urlencoded({extended : true}))   // to parse form data(urlencoded)
@@ -28,6 +29,13 @@ app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/notification", notificationRoutes)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`\nServer running at http://localhost:${process.env.PORT}`)
